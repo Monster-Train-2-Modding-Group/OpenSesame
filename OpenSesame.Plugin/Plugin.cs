52ef84cc92240e33de44d61294031da2256facd7
Brandon <tricksterguy87@gmail.com>
2025-06-17 00:23:28 -0700

    The files.

---
+++ OpenSesame.Plugin/Plugin.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using I2.Loc;
+using Microsoft.Extensions.Configuration;
+using ShinyShoe.Logging;
+using SimpleInjector;
+using TrainworksReloaded.Base;
+using TrainworksReloaded.Base.Card;
+using TrainworksReloaded.Base.CardUpgrade;
+using TrainworksReloaded.Base.Character;
+using TrainworksReloaded.Base.Class;
+using TrainworksReloaded.Base.Effect;
+using TrainworksReloaded.Base.Localization;
+using TrainworksReloaded.Base.Prefab;
+using TrainworksReloaded.Base.Trait;
+using TrainworksReloaded.Base.Trigger;
+using TrainworksReloaded.Core;
+using TrainworksReloaded.Core.Impl;
+using TrainworksReloaded.Core.Interfaces;
+using TrainworksReloaded.Core.Extensions;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace OpenSesame.Plugin
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger = new(MyPluginInfo.PLUGIN_GUID);
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(MetagameSaveData), nameof(MetagameSaveData.HasDiscoveredCard), [typeof(CardData)])]
+    class ShowAllLogbookCardsPatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MetagameSaveData), nameof(MetagameSaveData.HasDiscoveredCard), [typeof(string)])]
+    class ShowAllLogbookCardsPatch2
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MetagameSaveData), nameof(MetagameSaveData.HasDiscoveredRelic), [typeof(RelicData)])]
+    class ShowAllLogbookRelicsPatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MetagameSaveData), nameof(MetagameSaveData.HasDiscoveredChampionUpgrade), [typeof(CardUpgradeData)])]
+    class ShowAllLogbookUpgradePatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
